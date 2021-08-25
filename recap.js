96a989e7fe8adf568c1bdba9a48fde6210ed708c
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-08-25 08:37:56 +0600

    recap.js file handling

---
+++ recap.js
@@ -0,0 +1,5 @@
+var favoriteBook = '4 hours work book';
+var bookList = ['positioning', 'hooked', 'start with why', 'shoe dog'];
+var shoeDogIndex = bookList.indexOf('shoe dog');
+
+console.log(shoeDogIndex);
