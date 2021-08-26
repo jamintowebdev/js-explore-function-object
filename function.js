df21b86321ed2d548a23b4464e05f14c405abf39
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-08-26 10:51:56 +0600

    function.js dealing with function ... function declaration and function calling

---
+++ function.js
@@ -0,0 +1,21 @@
+// function declaration
+function startFan() {
+    console.log('walk towards the switch');
+    console.log('press the switch'); 
+}
+
+// Call the function
+startFan();
+
+console.log('eat breakfast');
+console.log('drink tea');
+console.log('take a shower');
+startFan();
+
+// you can use/call function as many time as you need.
+
+var country = 'BD';
+var city = 'Netrokona';
+var location = 'Barhatta';
+
+startFan();
