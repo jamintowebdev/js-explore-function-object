ea51f71c22d30c059677b78101aa3abc75d1a5ea
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-08-26 17:48:35 +0600

    object learned and practiced

---
+++ object.js
@@ -0,0 +1,25 @@
+var studentOne = {
+    id: 15,
+    name: 'Jahangir Alom Minto',
+    marks: 81,
+    phone: +8801747825881,
+};
+
+
+var mobile = {
+    color: 'black',
+    price: 20000,
+    screenSize: 6,
+    storage: '16GB'
+};
+
+
+var book = {
+    title: 'Start With Why',
+    author: 'Simon Sinek',
+    price: 200,
+    page: 400,
+    publisher: 'Penguin',
+}
+
+console.log(mobile);
